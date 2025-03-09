d5289950ff7f288e588127e9dc4eddc9b41e0218
DanielBoigk <dn.boigk@gmail.com>
2025-03-09 20:05:05 +0100

    do EITFullData

--- src/GenerateFullData/EITFullData.jl
+++ src/GenerateFullData/EITFullData.jl
@@ -5,16 +5,23 @@ struct EITFullData
     op::EITOperatorData
 
 
-    function EITFullData()
+    function EITFullData(path::String= "", σ=1.0, mode="exp")
         #load mesh of some kind
-
-
+        # maybe check if there is a path given if not
+        if path==""
+            #Do Something
+            # Just load standard path.
+            mesh = GmshDiscreteModel(path)
+        else
+            mesh = GmshDiscreteModel(path)
+        end
         # if given no function look at given sigma and mode and generate interpolable sigma function
 
 
-
+        conductivity = ...
         # Now generate the Operator from that
 
+        op = EITOperatorData(mesh,conductivity)
 
 
         # now generate a basis and solve
