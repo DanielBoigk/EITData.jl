ef9b3b728674dc35858531fdfdb499ea0a82e812
DanielBoigk <dn.boigk@gmail.com>
2025-03-11 19:52:51 +0100

    add Export Data

---
+++ src/Export/Export.jl
@@ -0,0 +1,16 @@
+# Please write a function that can export the entire EITFullData struct  into a file and save it at a specified location.
+
+using Serialization
+
+function save_eit_full_data(data::EITFullData, filename::String)
+    open(filename, "w") do io
+        serialize(io, data)
+    end
+end
+
+
+function load_eit_full_data(filename::String)
+    open(filename, "r") do io
+        return deserialize(io)
+    end
+end
