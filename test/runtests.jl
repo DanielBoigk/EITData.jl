8735deb1feddd6bf9fe75161a886bca5042fdf1b
DanielBoigk <dn.boigk@gmail.com>
2025-03-12 02:07:24 +0100

    first try instantiate

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,5 @@ Pkg.activate(@__DIR__)   # This ensures the test environment is active
 
 using Test
 using EITData   # or "using .EITData" if it's a local module
+
+@test isdefined(EITData, :default_func)
