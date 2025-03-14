c90c69aae2a98fa9e20fff201830d302d9834a55
DanielBoigk <dn.boigk@gmail.com>
2025-03-14 02:23:56 +0100

    add License

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
-using Pkg
-Pkg.activate(@__DIR__)   # This ensures the test environment is active
+#using Pkg
+#Pkg.activate(@__DIR__)   # This ensures the test environment is active
 
 using Test
 using EITData   # or "using .EITData" if it's a local module
