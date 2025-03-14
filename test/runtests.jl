ec77de6e624779445ba88042eababdb4bca98d38
DanielBoigk <dn.boigk@gmail.com>
2025-03-14 02:00:31 +0100

    fix bugs

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,4 +4,6 @@ Pkg.activate(@__DIR__)   # This ensures the test environment is active
 using Test
 using EITData   # or "using .EITData" if it's a local module
 
-@test isdefined(EITData, :default_func)
+@testset "EITData.jl" begin
+    @test isdefined(EITData, :default_func)
+end
