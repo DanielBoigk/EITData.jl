06208deff47cb865d2bfa08f6e51102228cd002e
DanielBoigk <dn.boigk@gmail.com>
2025-03-19 00:07:33 +0100

    add Minimal pairs

--- src/MinimalPairs/MinimallPairs.jl
+++ src/MinimalPairs/MinimallPairs.jl
@@ -1,7 +1,19 @@
 
 
 struct MinimalPair
+    dim::Int
+    # coordinates of the mesh
     boundary_points::Array{Float64,2}
     interior_points::Array{Float64,2}
-    γ_boundary::
+    γ_boundary::Array{Float64,1}
+    γ_interior::Array{Float64,1}
+    g_boundary::Array{Float64,1}
+    f_boundary::Array{Float64,1}
+    u_f::Array{Float64,1}
+    u_g::Array{Float64,1}
+    gradient_boundary::Array{Float64,2}
+    gradient_interior::Array{Float64,2}
+    error_L2::Float64
+    error_Linf::Float64
+    error_wasserstein::Float64
 end
